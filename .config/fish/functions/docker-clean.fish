1240d572f18f0cbb1f6cf048dfd90da135b1a180
Benedikt Ritter <beneritter@gmail.com>
2016-11-29 16:41:52 +0100

    Fix broken fish function

--- .config/fish/functions/docker-clean.fish
+++ .config/fish/functions/docker-clean.fish
@@ -1,5 +1,5 @@
 function docker-clean
   docker rm (docker ps -q -f 'status=exited')
   docker rmi (docker images -q -f "dangling=true")
-  docker volume rm (docker volume ls -qf dangling=true
+  docker volume rm (docker volume ls -qf dangling=true)
 end
