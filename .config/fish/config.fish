eedf8a6aaba7a7b6cecc6187cfde9bf6e9a5cc72
Benedikt Ritter <beneritter@gmail.com>
2016-11-29 12:41:01 +0100

    Activate rbenv in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,11 @@ set -x GOPATH ~/.gopath
 
 set PATH /usr/local/bin /usr/local/sbin $JAVA_HOME/bin ~/bin $PATH
 
+## rbenv support
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
+
 set -x MAVEN_OPTS "-Xmx512m -XX:MaxPermSize=128m"
 
 set -x SVN_EDITOR vim
