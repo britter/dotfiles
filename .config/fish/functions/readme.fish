8976fc6147324dba5ef5c39be3598d80c26803a9
Benedikt Ritter <beneritter@gmail.com>
2017-04-14 12:30:31 +0200

    Convert readme function to fish

---
+++ .config/fish/functions/readme.fish
@@ -0,0 +1,16 @@
+function readme
+	set project_name (basename $PWD)
+    printf "\
+# $project_name
+
+Welcome to $project_name!
+
+## Contribution policy
+
+Contributions via GitHub pull requests are gladly accepted from their original author. Along with any pull requests, please state that the contribution is your original work and that you license the work to the project under the project's open source license. Whether or not you state this explicitly, by submitting any copyrighted material via pull request, email, or other means you agree to license the material under the project's open source license and warrant that you have the legal authority to do so.
+
+## License
+
+This code is open source software licensed under the [Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0.html).
+" | cat > README.md
+end
