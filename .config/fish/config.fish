8b4f2e03d9c133de417dc7ff628b4f044bfdee0d
Benedikt Ritter <beneritter@gmail.com>
2019-02-08 16:44:49 +0100

    Add additional Java home variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,18 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 ## SDKs ##
+set -x JAVA_6_HOME (/usr/libexec/java_home -v 1.6)
+set -x JDK6 $JAVA_6_HOME
 set -x JAVA_7_HOME (/usr/libexec/java_home -v 1.7)
+set -x JDK7 $JAVA_7_HOME
 set -x JAVA_8_HOME (/usr/libexec/java_home -v 1.8)
+set -x JDK8 $JAVA_8_HOME
 set -x JAVA_9_HOME (/usr/libexec/java_home -v 9)
+set -x JDK9 $JAVA_9_HOME
 set -x JAVA_10_HOME (/usr/libexec/java_home -v 10)
+set -x JDK10 $JAVA_10_HOME
 set -x JAVA_11_HOME (/usr/libexec/java_home -v 11)
+set -x JDK11 $JAVA_11_HOME
 set -x JAVA_HOME $JAVA_11_HOME
 
 set -x GOPATH ~/.gopath
