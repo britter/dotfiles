6105b1ede5eae8a6f3f6f195dc6284ded212fa38
Benedikt Ritter <beneritter@gmail.com>
2020-01-06 15:32:25 +0100

    Add Java 13

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ set -x LC_ALL C
 set -x JDK8 "$SDKMAN_CANDIDATES_DIR/java/8.0.232.hs-adpt"
 set -x JDK11 "$SDKMAN_CANDIDATES_DIR/java/11.0.5.hs-adpt"
 set -x JDK12 "$SDKMAN_CANDIDATES_DIR/java/12.0.2.hs-adpt"
+set -x JDK13 "$SDKMAN_CANDIDATES_DIR/java/13.0.1.hs-adpt"
 set -x JAVA_HOME $JDK11
 
 set -x GOPATH ~/.gopath
