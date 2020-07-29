d45e1dd69f7aa7b7b9cc3796b25baeafbc45f7c6
Benedikt Ritter <beneritter@gmail.com>
2020-07-29 11:12:02 +0200

    Update all the JDKs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,11 +8,11 @@ alias cat=bat
 set -x LANG en_US.utf-8
 
 ## SDKs ##
-set -x JDK6 "$SDKMAN_CANDIDATES_DIR/java/1.6.65"
-set -x JDK8 "$SDKMAN_CANDIDATES_DIR/java/8.0.232.hs-adpt"
-set -x JDK11 "$SDKMAN_CANDIDATES_DIR/java/11.0.5.hs-adpt"
+set -x JDK8 "$SDKMAN_CANDIDATES_DIR/java/8.0.262.hs-adpt"
+set -x JDK11 "$SDKMAN_CANDIDATES_DIR/java/11.0.8.hs-adpt"
 set -x JDK12 "$SDKMAN_CANDIDATES_DIR/java/12.0.2.hs-adpt"
-set -x JDK13 "$SDKMAN_CANDIDATES_DIR/java/13.0.1.hs-adpt"
+set -x JDK13 "$SDKMAN_CANDIDATES_DIR/java/13.0.2.hs-adpt"
+set -x JDK14 "$SDKMAN_CANDIDATES_DIR/java/14.0.2.hs-adpt"
 set -x JAVA_HOME $JDK11
 
 set -x GOPATH ~/.gopath
