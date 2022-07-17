1ebb41e2196f306f51bdb235aa280bcf353cbab4
Benedikt Ritter <beneritter@gmail.com>
2022-07-17 21:53:09 +0200

    Add asdf specific configuration for java installations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,16 +7,6 @@ alias cat=bat
 
 set -x LANG en_US.utf-8
 
-## SDKs ##
-set -x JDK8 "$SDKMAN_CANDIDATES_DIR/java/8.0.282.hs-adpt"
-set -x JDK11 "$SDKMAN_CANDIDATES_DIR/java/11.0.10.hs-adpt"
-set -x JDK12 "$SDKMAN_CANDIDATES_DIR/java/12.0.2.hs-adpt"
-set -x JDK13 "$SDKMAN_CANDIDATES_DIR/java/13.0.2.hs-adpt"
-set -x JDK14 "$SDKMAN_CANDIDATES_DIR/java/14.0.2.hs-adpt"
-set -x JDK15 "$SDKMAN_CANDIDATES_DIR/java/15.0.2.hs-adpt"
-set -x JDK16 "$SDKMAN_CANDIDATES_DIR/java/16.0.0.hs-adpt"
-set -x JAVA_HOME $JDK11
-
 set -x MAVEN_OPTS "-Duser.name=benedikt"
 
 set -x GOPATH ~/.gopath
@@ -30,6 +20,12 @@ set -x GPG_TTY (tty)
 
 # asdf scripts need to be sourced after you have set your $PATH and after you have sourced your framework (oh-my-zsh etc).
 source /opt/asdf-vm/asdf.fish
+. ~/.asdf/plugins/java/set-java-home.fish
+
+set -x JDK8 (asdf where java liberica-8u333+2)
+set -x JDK11 (asdf where java liberica-11.0.15.1+2)
+set -x JDK16 (asdf where java liberica-16.0.2+7)
+set -x JDK17 (asdf where java liberica-17.0.3.1+2)
 
 # init prompt
 starship init fish | source
