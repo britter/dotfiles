962c0c1dd7f837b7373b3dfa13ce8411fa3d1096
Benedikt Ritter <beneritter@gmail.com>
2022-07-18 15:37:13 +0200

    Drop SDKman completely

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,7 +19,13 @@ set -x SVN_EDITOR vim
 set -x GPG_TTY (tty)
 
 # asdf scripts need to be sourced after you have set your $PATH and after you have sourced your framework (oh-my-zsh etc).
-source /opt/asdf-vm/asdf.fish
+switch (uname)
+  case Linux
+    source /opt/asdf-vm/asdf.fish
+  case Darwin
+    echo -e "\nsource "(brew --prefix asdf)"/libexec/asdf.fish" >> ~/.config/fish/config.fish
+end
+# on macOS additional configuration is required in .asdfrc, see https://github.com/halcyon/asdf-java#macos
 . ~/.asdf/plugins/java/set-java-home.fish
 
 set -x JDK8 (asdf where java liberica-8u333+2)
@@ -29,3 +35,9 @@ set -x JDK17 (asdf where java liberica-17.0.3.1+2)
 
 # init prompt
 starship init fish | source
+
+source /usr/local/opt/asdf/libexec/asdf.fish
+
+source /usr/local/opt/asdf/libexec/asdf.fish
+
+source /usr/local/opt/asdf/libexec/asdf.fish
