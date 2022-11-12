c0ab8db1d70b9d4d1ada6e1d4e54c2f5cbfc9796
Benedikt Ritter <benedikt@gradle.com>
2022-11-12 12:28:29 +0100

    Add JDK 18

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,6 +38,7 @@ set -x JDK8 (asdf where java liberica-8u333+2)
 set -x JDK11 (asdf where java liberica-11.0.15.1+2)
 set -x JDK16 (asdf where java liberica-16.0.2+7)
 set -x JDK17 (asdf where java liberica-17.0.3.1+2)
+set -x JDK18 (asdf where java liberica-18.0.2.1+1)
 
 # init prompt
 starship init fish | source
